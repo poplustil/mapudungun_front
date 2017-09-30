62945ff182d3bcb5a8a5e46660cec7148a1215d2
poplustil <pilar.oplust@gmail.com>
2017-09-30 12:01:01 -0300

    avances

--- front_end.py
+++ front_end.py
@@ -1,9 +1,24 @@
 # -*- coding: utf-8 -*-
 import json
 import codecs
+from collections import defaultdict
+
+###### CLASES ######
+
+# defalut dict: Por ahora, si el grafema no esta en LTS, devuelve el mismo valor
+class defDict(dict):
+    __missing__ = lambda self, key: key[0]
+
+# data structure por oracion
+class lingAnalisis(sentence):
+    pass
+
+###### FUNCIONES #####
+
 # pipeline front-end
 
 # diccionario pronunciacion de letras aisladas
+# TODO
 
 # funcion para normalizacion de numeros
 # input: numero
@@ -18,12 +33,13 @@ def normalizeNumbers(number_word):
 # input: archivo de texto con el siguiente formato
 # output: una estructura de datos que usara la funcion de transcripcion fonetica
 def getLTS(lts_file):
-    lts_dict = {}
+    lts_dict = defDict()
     digraphs = []
     with codecs.open(lts_file, encoding='utf-8') as data_file:
         for line in data_file:
             k1, k2, val = line.split()
             lts_dict[(k1, k2)] = val
+            print val
 
     [digraphs.append(n[0]) for n in lts_dict.keys() if len(n[0]) > 1 and n[0] not in digraphs]
 
@@ -32,33 +48,46 @@ def getLTS(lts_file):
 # funcion para hacer transcripcion fonetica
 # input: palabra, grafemario, lista de digrafos
 # output: transcripcion fonetica de una palabra
+# TODO: que pasa con las palabras en español?
 def transcribe(word, graph_inv, lts_dict, digraph_list):
+    PUNCT = '.,;:'
     phones = []
     flag = False
     for n in range(0, len(word)):
-        if n != len(word)-1:
-            if flag == True:
-                flag = False
-            elif word[n]+word[n+1] not in digraph_list and flag == False:
-                phones.append(lts_dict[(word[n],graph_inv)])
+        if word[n] not in PUNCT:
+            if n != len(word)-1:
+                if flag == True:
+                    flag = False
+                elif word[n]+word[n+1] not in digraph_list and flag == False:
+                    phones.append(lts_dict[(word[n],graph_inv)])
+                else:
+                    phones.append(lts_dict[(word[n]+word[n+1], graph_inv)])
+                    flag = True
             else:
-                phones.append(lts_dict[(word[n]+word[n+1], graph_inv)])
-                flag = True
-        else:
-            phones.append(lts_dict[(word[n],graph_inv)])
-    print phones
+                phones.append(lts_dict[(word[n],graph_inv)])
+    return phones
 
-lts_dict, digraphs = getLTS('LTS')
-print lts_dict, digraphs
-transcribe(u'trawa', 'unificado', lts_dict, digraphs)
-
-# reglas de silibifacion
-# input: archivo de texto con el siguiente formato
-# output: una estructura de datos que usara la funcion de silibificacion
-
+# TODO: identificar palabras en español para insertar schwa en grupos consonanticos en ataque
 # funcion para silibificar
 # input: transcripcion fonetica de una palabra
 # output: transcripcion fonetica silibificada de una palabra
+def syllabify(phone_list):
+    C = ('f', 'k', 'l', 'lh', 'll', 'm', 'n', 'nh', 'ny', 'ng', 'p', 'r','s', 't', 'th', 'ch', 'tx')
+    V = ('a', 'e' 'i', 'o', 'u', 'y')
+    semi = ('j', 'w')
+    oclAfr = ('ch', 'tx', 'p', 't', 'th', 'k')
+    g = 'g'
+    posSeq = ('V', 'VC', 'CV', 'CVC')
+
+    syllabified = []
+    for n in range(0, len(phone_list)):
+        c = phone_list[n]
+        p = phone_list[n-1]
+        x = phone_list[n+1]
+
+
+
+
 
 # reglas de acentuacion
 # input: archivo de texto con el siguiente formato
@@ -87,6 +116,14 @@ transcribe(u'trawa', 'unificado', lts_dict, digraphs)
 #---- Funciones benri
 
 # funcion que tiene como input un corpus y selecciona las oraciones con mayor coverage
+def getScript(corpus_path):
+    text = codecs.open(corpus_path, encoding='utf-8').read().split('\n')
+    lts_dict, digraphs = getLTS('LTS')
+    for n in text:
+        for i in n.split():
+            phones = transcribe(i.lower(), 'azumfeche', lts_dict, digraphs)
+            print syllabify(phones)
+
 
 # funcion para crear un MLF a partir del script
 
@@ -95,3 +132,7 @@ transcribe(u'trawa', 'unificado', lts_dict, digraphs)
 # funcion que mide el coverage de un mlf
 
 # funcion para crear etiquetas HTS para cada oracion
+
+
+#### MAIN ####
+getScript('textosmapuches_primerobasico.txt')
